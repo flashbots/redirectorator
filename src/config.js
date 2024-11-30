176e7cd2a687364090cda391aefb2f17b4fbbf42
Daniel Sukoneck <daniel@sukoneck.com>
2024-11-29 23:47:41 -0700

    html in web response

--- src/config.js
+++ src/config.js
@@ -11,7 +11,7 @@ export const config = {
       host: "blocks.flashbots.net",
       path: "/",
       type: "web",
-      message: "This API has been decommissioned. You can visit datasets.flashbots.net for historical data. Join us on the Flashbots forum.",
+      message: "<p>This API has been decommissioned. You can visit <a href='https://datasets.flashbots.net'>datasets.flashbots.net</a> for historical data. </p><p>Join us on the Flashbots forum 👉 ",
       redirectUrl: "https://collective.flashbots.net/",
       status: 410
     },
