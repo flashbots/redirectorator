29e6710ee7d35cbe8c7f368a93f21ea4807b4142
sukoneck <19413126+sukoneck@users.noreply.github.com>
2025-05-13 11:13:18 -0700

    Add suave (#2)

--- src/config.js
+++ src/config.js
@@ -1,7 +1,23 @@
 export const config = {
   "defaultStatus": 410,
   "hosts": {
-    "suave-alpha.flashbots.net": {
+    "blocks.flashbots.net": {
+      routes: [
+        {
+          path: "/",
+          response: {
+            api: {
+              message: "This API has been decommissioned. You can visit datasets.flashbots.net for historical data and join us at collective.flashbots.net"
+            },
+            web: {
+              message: "This API has been decommissioned. You can visit <a href='https://datasets.flashbots.net'>datasets.flashbots.net</a> for historical data. Join us at the Flashbots forum.",
+              redirectUrl: "https://collective.flashbots.net/"
+            },
+          },
+        },
+      ]
+    },
+    "suave.flashbots.net": {
       routes: [
         {
           path: "/",
@@ -17,17 +33,17 @@ export const config = {
         },
       ]
     },
-    "blocks.flashbots.net": {
+    "suave-alpha.flashbots.net": {
       routes: [
         {
           path: "/",
           response: {
             api: {
-              message: "This API has been decommissioned. You can visit datasets.flashbots.net for historical data and join us at collective.flashbots.net"
+              message: "This website has been decommissioned. Please visit writings.flashbots.net and join us at collective.flashbots.net"
             },
             web: {
-              message: "This API has been decommissioned. You can visit <a href='https://datasets.flashbots.net'>datasets.flashbots.net</a> for historical data. Join us at the Flashbots forum.",
-              redirectUrl: "https://collective.flashbots.net/"
+              message: "This website has been decommissioned. Please join us at the <a href='https://collective.flashbots.net'>Flashbots forum</a> and visit <a href='https://writings.flashbots.net'>writings.flashbots.net</a>.",
+              redirectUrl: "https://writings.flashbots.net/"
             },
           },
         },
