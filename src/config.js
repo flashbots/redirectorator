4aa3643be001b00dc6bf9f806c4ba85c785340e8
Daniel Sukoneck <daniel@sukoneck.com>
2025-07-01 23:48:15 +0200

    Add transparency and explore

--- src/config.js
+++ src/config.js
@@ -17,6 +17,22 @@ export const config = {
         },
       ]
     },
+    "explore.flashbots.net": {
+      routes: [
+        {
+          path: "/",
+          response: {
+            api: {
+              message: "This website has been decommissioned. Please visit writings.flashbots.net and join us at collective.flashbots.net"
+            },
+            web: {
+              message: "This website has been decommissioned. Please join us at the <a href='https://collective.flashbots.net'>Flashbots forum</a> and visit <a href='https://writings.flashbots.net'>writings.flashbots.net</a>.",
+              redirectUrl: "https://writings.flashbots.net/"
+            },
+          },
+        },
+      ]
+    },
     "suave.flashbots.net": {
       routes: [
         {
@@ -63,6 +79,22 @@ export const config = {
               redirectDelay: 5,
               redirectUrl: "http://localhost:8787/",
               status: 410,
+            },
+          },
+        },
+      ]
+    },
+    "transparency.flashbots.net": {
+      routes: [
+        {
+          path: "/",
+          response: {
+            api: {
+              message: "This website has been decommissioned. Please visit writings.flashbots.net and join us at collective.flashbots.net"
+            },
+            web: {
+              message: "This website has been decommissioned. Please join us at the <a href='https://collective.flashbots.net'>Flashbots forum</a> and visit <a href='https://writings.flashbots.net'>writings.flashbots.net</a>.",
+              redirectUrl: "https://writings.flashbots.net/"
             },
           },
         },
