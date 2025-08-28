0eced19ba60fc934b3893d9e6ea4ae5a1acd64f9
Ilya Lukyanov <73885545+ilyaluk@users.noreply.github.com>
2025-08-28 15:43:14 +0100

    Unify redirects, add datasets.flashbots.net

--- src/config.js
+++ src/config.js
@@ -1,70 +1,29 @@
-export const config = {
+var defaultRedirectConfig = {
+  routes: [
+    {
+      path: "/",
+      response: {
+        api: {
+          message: "This website has been decommissioned. Please visit writings.flashbots.net and join us at collective.flashbots.net"
+        },
+        web: {
+          message: "This website has been decommissioned. Please join us at the <a href='https://collective.flashbots.net'>Flashbots forum</a> and visit <a href='https://writings.flashbots.net'>writings.flashbots.net</a>.",
+          redirectUrl: "https://writings.flashbots.net/"
+        }
+      }
+    }
+  ]
+};
+
+var config = {
   "defaultStatus": 410,
   "hosts": {
-    "blocks.flashbots.net": {
-      routes: [
-        {
-          path: "/",
-          response: {
-            api: {
-              message: "This API has been decommissioned. You can visit datasets.flashbots.net for historical data and join us at collective.flashbots.net"
-            },
-            web: {
-              message: "This API has been decommissioned. You can visit <a href='https://datasets.flashbots.net'>datasets.flashbots.net</a> for historical data. Join us at the Flashbots forum.",
-              redirectUrl: "https://collective.flashbots.net/"
-            },
-          },
-        },
-      ]
-    },
-    "explore.flashbots.net": {
-      routes: [
-        {
-          path: "/",
-          response: {
-            api: {
-              message: "This website has been decommissioned. Please visit writings.flashbots.net and join us at collective.flashbots.net"
-            },
-            web: {
-              message: "This website has been decommissioned. Please join us at the <a href='https://collective.flashbots.net'>Flashbots forum</a> and visit <a href='https://writings.flashbots.net'>writings.flashbots.net</a>.",
-              redirectUrl: "https://writings.flashbots.net/"
-            },
-          },
-        },
-      ]
-    },
-    "suave.flashbots.net": {
-      routes: [
-        {
-          path: "/",
-          response: {
-            api: {
-              message: "This website has been decommissioned. Please visit writings.flashbots.net and join us at collective.flashbots.net"
-            },
-            web: {
-              message: "This website has been decommissioned. Please join us at the <a href='https://collective.flashbots.net'>Flashbots forum</a> and visit <a href='https://writings.flashbots.net'>writings.flashbots.net</a>.",
-              redirectUrl: "https://writings.flashbots.net/"
-            },
-          },
-        },
-      ]
-    },
-    "suave-alpha.flashbots.net": {
-      routes: [
-        {
-          path: "/",
-          response: {
-            api: {
-              message: "This website has been decommissioned. Please visit writings.flashbots.net and join us at collective.flashbots.net"
-            },
-            web: {
-              message: "This website has been decommissioned. Please join us at the <a href='https://collective.flashbots.net'>Flashbots forum</a> and visit <a href='https://writings.flashbots.net'>writings.flashbots.net</a>.",
-              redirectUrl: "https://writings.flashbots.net/"
-            },
-          },
-        },
-      ]
-    },
+    "blocks.flashbots.net": defaultRedirectConfig,
+    "explore.flashbots.net": defaultRedirectConfig,
+    "suave.flashbots.net": defaultRedirectConfig,
+    "suave-alpha.flashbots.net": defaultRedirectConfig,
+    "transparency.flashbots.net": defaultRedirectConfig,
+    "datasets.flashbots.net": defaultRedirectConfig,
     "test": {
       routes: [
         {
@@ -72,32 +31,16 @@ export const config = {
           response: {
             api: {
               message: "api message",
-              status: 410,
+              status: 410
             },
             web: {
               message: "Web message.",
               redirectDelay: 5,
               redirectUrl: "http://localhost:8787/",
-              status: 410,
-            },
-          },
-        },
-      ]
-    },
-    "transparency.flashbots.net": {
-      routes: [
-        {
-          path: "/",
-          response: {
-            api: {
-              message: "This website has been decommissioned. Please visit writings.flashbots.net and join us at collective.flashbots.net"
-            },
-            web: {
-              message: "This website has been decommissioned. Please join us at the <a href='https://collective.flashbots.net'>Flashbots forum</a> and visit <a href='https://writings.flashbots.net'>writings.flashbots.net</a>.",
-              redirectUrl: "https://writings.flashbots.net/"
-            },
-          },
-        },
+              status: 410
+            }
+          }
+        }
       ]
     }
   }
