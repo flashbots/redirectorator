9570f9f6d08c5a204ea4ed03d6fc277cde1aeb11
Ilya Lukyanov <73885545+ilyaluk@users.noreply.github.com>
2025-08-29 17:12:54 +0100

    fix config export

--- src/config.js
+++ src/config.js
@@ -4,44 +4,48 @@ var defaultRedirectConfig = {
       path: "/",
       response: {
         api: {
-          message: "This website has been decommissioned. Please visit writings.flashbots.net and join us at collective.flashbots.net"
+          message:
+            "This website has been decommissioned. Please visit writings.flashbots.net and join us at collective.flashbots.net",
         },
         web: {
-          message: "This website has been decommissioned. Please join us at the <a href='https://collective.flashbots.net'>Flashbots forum</a> and visit <a href='https://writings.flashbots.net'>writings.flashbots.net</a>.",
-          redirectUrl: "https://writings.flashbots.net/"
-        }
-      }
-    }
-  ]
+          message:
+            "This website has been decommissioned. Please join us at the <a href='https://collective.flashbots.net'>Flashbots forum</a> and visit <a href='https://writings.flashbots.net'>writings.flashbots.net</a>.",
+          redirectUrl: "https://writings.flashbots.net/",
+        },
+      },
+    },
+  ],
 };
 
 var config = {
-  "defaultStatus": 410,
-  "hosts": {
+  defaultStatus: 410,
+  hosts: {
     "blocks.flashbots.net": defaultRedirectConfig,
     "explore.flashbots.net": defaultRedirectConfig,
     "suave.flashbots.net": defaultRedirectConfig,
     "suave-alpha.flashbots.net": defaultRedirectConfig,
     "transparency.flashbots.net": defaultRedirectConfig,
     "datasets.flashbots.net": defaultRedirectConfig,
-    "test": {
+    test: {
       routes: [
         {
           path: "/",
           response: {
             api: {
               message: "api message",
-              status: 410
+              status: 410,
             },
             web: {
               message: "Web message.",
               redirectDelay: 5,
               redirectUrl: "http://localhost:8787/",
-              status: 410
-            }
-          }
-        }
-      ]
-    }
-  }
+              status: 410,
+            },
+          },
+        },
+      ],
+    },
+  },
 };
+
+export { config };
